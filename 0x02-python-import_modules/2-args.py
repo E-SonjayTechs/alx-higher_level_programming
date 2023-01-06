eca9a52344dbf74ddd2a735ae39578b26db1e497
E-SonjayTechs <sonjayben@gmail.com>
2023-01-06 08:47:39 -0800

    task 2 updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+
     i = len(sys.argv) - 1
 
     if i == 0:
-        print("{} arguments:".format(i))
+        print("{} arguments.".format(i))
     elif i == 1:
-        print("{} arguments:".format(i))
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
